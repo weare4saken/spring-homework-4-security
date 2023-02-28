8a23909daaef3a935138ea4e318224e05684d4e6
weare4saken <kondrashka91@gmail.com>
2023-02-28 16:53:57 +0700

    Forth commit: Spring Security Homework DONE

--- src/main/java/weare4saken/config/ConfigNew.java
+++ src/main/java/weare4saken/config/ConfigNew.java
@@ -1,11 +1,15 @@
 package weare4saken.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "weare4saken")
@@ -20,6 +24,22 @@ public class ConfigNew {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    @Bean
+    public DataSource getDataSource() throws PropertyVetoException {
+
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://localhost/skypro");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("828631");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        return dataSource;
     }
 
 }
--- src/main/java/weare4saken/config/SecurityConfigNew.java
+++ src/main/java/weare4saken/config/SecurityConfigNew.java
@@ -6,19 +6,28 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfigNew extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+    public SecurityConfigNew(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+        /*User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
         auth.inMemoryAuthentication()
                 .withUser(userBuilder.username("Liza").password("qwerty").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("Katya").password("asdfg").roles("IT"))
                 .withUser(userBuilder.username("Alena").password("zxcvb").roles("STUFF"))
-                .withUser(userBuilder.username("Tanya").password("12345").roles("DIRECTOR"));
+                .withUser(userBuilder.username("Tanya").password("12345").roles("DIRECTOR"));*/
     }
 
     @Override
